33caca12e4b3ab3cecac416263c88c1fcd6f6d29
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2022-04-29 09:45:47 +0100

    Refactor code (#1)
    * Refactor code

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -6,13 +6,5 @@
 actions :add, :remove
 default_action :add
 
-attribute :cores, :kind_of => Integer, :default => 1
-attribute :memory_kb, :kind_of => Integer, :default => 102400
-attribute :enrichment_enabled, :kind_of => [TrueClass, FalseClass], :default => true
-attribute :cache_dir, :kind_of => String, :default => "/var/cache/snort"
-attribute :templates_dir, :kind_of => String, :default => "/var/cache/snort/templates"
-attribute :config_dir, :kind_of => String, :default => "/etc/snort"
-attribute :user, :kind_of => String, :default => "snort"
-
-
 attribute :sensor_id, :kind_of => Integer, :default => 0
+attribute :groups, :kind_of => Array, :default => []
